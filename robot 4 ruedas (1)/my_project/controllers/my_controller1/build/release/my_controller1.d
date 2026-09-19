ab8804a348df0fbcce8a93e0b535a7e182485335
ivonne arizabalo <ivonnesanchez20619@gmail.com>
2026-09-18 21:56:44 -0600

    tut

---
+++ robot 4 ruedas (1)/my_project/controllers/my_controller1/build/release/my_controller1.d
@@ -0,0 +1,6 @@
+build/release/my_controller1.o: my_controller1.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
---
+++ robot 4 ruedas (1)/my_project/controllers/my_controller1/my_controller1.c
@@ -0,0 +1,60 @@
+#include <webots/distance_sensor.h>
+#include <webots/motor.h>
+#include <webots/robot.h>
+
+#define TIME_STEP 64
+
+int main(int argc, char **argv) {
+  wb_robot_init();
+  
+  int i;
+  int avoid_obstacle_counter = 0; // Cambiado a int para contar los pasos
+
+  // Inicialización de sensores de distancia
+  WbDeviceTag ds[2];
+  char ds_names[2][10] = {"ds_left", "ds_right"};
+  for (i = 0; i < 2; i++) {
+    ds[i] = wb_robot_get_device(ds_names[i]);
+    wb_distance_sensor_enable(ds[i], TIME_STEP);
+  }
+
+  // Inicialización de motores de las ruedas
+  WbDeviceTag wheels[4];
+  char wheels_names[4][8] = {"wheel1", "wheel2", "wheel3", "wheel4"};
+  for (i = 0; i < 4; i++) {
+    wheels[i] = wb_robot_get_device(wheels_names[i]);
+    wb_motor_set_position(wheels[i], INFINITY);
+  }
+
+  // Bucle principal de simulación
+  while (wb_robot_step(TIME_STEP) != -1) {
+    double left_speed = 1.0;
+    double right_speed = 1.0;
+
+    if (avoid_obstacle_counter > 0) {
+      avoid_obstacle_counter--;
+      left_speed = 1.0;
+      right_speed = -1.0;
+    } else { 
+      // Leer valores de los sensores
+      double ds_values[2];
+      for (i = 0; i < 2; i++) {
+        ds_values[i] = wb_distance_sensor_get_value(ds[i]);
+      }
+
+      // Si detecta un obstáculo cerca, activa la maniobra de giro
+      if (ds_values[0] < 950.0 || ds_values[1] < 950.0) {
+        avoid_obstacle_counter = 100;
+      }
+    }
+
+    // Aplicar velocidades a las 4 ruedas
+    wb_motor_set_velocity(wheels[0], left_speed);
+    wb_motor_set_velocity(wheels[1], right_speed);
+    wb_motor_set_velocity(wheels[2], left_speed);
+    wb_motor_set_velocity(wheels[3], right_speed);
+  }
+
+  wb_robot_cleanup();
+  return 0;
+}
